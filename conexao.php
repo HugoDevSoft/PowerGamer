42ea741208a85408c7a99779bff1b3d178f17429
HugoDevSoft <hugaogola@gmail.com>
2025-06-26 23:49:34 -0300

    ajuste mapa

--- conexao.php
+++ conexao.php
@@ -2,4 +2,4 @@
 
 //host, usuário, senha, nome do banco; Numero gerado free WHA, a senha que você criou para o banco, e o número novamanente do free WHA
 
-$conn = mysqli_connect('localhost', 'root', 'djd12123', 'netchute') or die('Erro ao conectar ao banco de dados')
+$conn = mysqli_connect('localhost', 'root', 'djd12123', 'netchute') or die('Erro ao conectar ao banco de dados');

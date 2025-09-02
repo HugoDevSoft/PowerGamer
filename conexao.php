7f47cc1bda081c4330b91e76ca27793e9a8d4e87
HugoDevSoft <hugaogola@gmail.com>
2025-09-01 23:35:24 -0300

    testando requisições na paginas cadastro e login

--- conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-//host, usuário, senha, nome do banco; Numero gerado free WHA, a senha que você criou para o banco, e o número novamanente do free WHA
-
-$conn = mysqli_connect('localhost', 'root', 'djd12123', 'netchute') or die('Erro ao conectar ao banco de dados');
